471a483a103b2f4472800734f1384c494eabaf09
Kristian Sik <krissik98@yandex.ua>
2018-10-01 15:15:47 +0300

    Dropdown onclick fixed

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $(".dropdown-btn").on("click", function(){
         $(".dropdown-content").fadeToggle();
     });
-    window.onclick = function(event) {
-        if (!(event.target.matches('.dropdown-btn') || (event.target.matches(".heart-icon")))) {
-            $(".dropdown-content").fadeOut();
-        }
-      }
+    // window.onclick = function(event) {
+    //     if (!(event.target.matches('.dropdown-btn') || event.target.matches(".heart-icon") || event.target.matches(".dropdown-content-share-it"))) {
+    //         $(".dropdown-content").fadeOut();
+    //     }
+    // }
 });;
